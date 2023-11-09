9dc70e7a471126b793b647beb0cd25e9d0880ba2
Mateus Santos <mateus.sanjo20@gmail.com>
2023-11-09 11:19:39 -0300

    Criação do código que escreve a mensagem Hello World!

---
+++ hello.asm
@@ -0,0 +1,13 @@
+section .text
+    global _start           ;precisa ser adicionado para o gerador do executavel saiba onde começa
+
+_start:
+    mov edx,len             ;tamanho do texto. len(length-> comprimento)
+    mov ecx,msg             ;mensagem de texto para exibir
+    mov ebx,1               ;Decritor de arquivo
+    mov eax,4               ;chamada para escrever o teto na tela
+    int 0x80                ;chama o terminal para exibir o texto
+
+section .data               ;seçaõ de dados 
+msg db 'Hello, world!'      ;mensagem que dee aparecer
+len equ $ - msg             ;obtem a quantidade de caracteres da mensagem
